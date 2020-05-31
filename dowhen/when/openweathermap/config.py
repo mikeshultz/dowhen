1ffcb462860bee016e2ee773389359d95563a1b7
Mike Shultz <shultzm@gmail.com>
2020-05-31 15:34:09 -0600

    allow yaml config of owm_api_key

--- dowhen/when/openweathermap/config.py
+++ dowhen/when/openweathermap/config.py
@@ -1,6 +1,9 @@
 import os
+from dowhen.config import init_config
 
-OWM_API_KEY = os.environ.get('OWM_API_KEY')
+_conf = init_config()
+
+OWM_API_KEY = os.environ.get('OWM_API_KEY', _conf.get('owm_api_key'))
 OWM_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 FORECAST_API = 'https://api.openweathermap.org/data/2.5/forecast?zip={zip},{country_code}&appid={api_key}&units=imperial'
 DEFAULT_ZIP = 90210
