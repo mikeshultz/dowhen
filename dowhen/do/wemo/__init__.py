2f2cafcc09c9de25f894ec42b366a55e40ada2ad
Mike Shultz <shultzm@gmail.com>
2020-06-14 11:52:19 -0600

    handle missing MAC addresses in wemo do module

--- dowhen/do/wemo/__init__.py
+++ dowhen/do/wemo/__init__.py
@@ -6,19 +6,34 @@ log = get_logger(__name__)
 
 def on(mac):
     """ Turn a Wemo device on """
+    if mac is None:
+        log.error('MAC address is None!')
+        return
+
     log.info('Turning on WeMo device {}'.format(mac))
+
     return device_on(mac)
 
 
 def off(mac):
     """ Turn a Wemo device off """
+    if mac is None:
+        log.error('MAC address is None!')
+        return
+
     log.info('Turning off WeMo device {}'.format(mac))
+
     return device_off(mac)
 
 
 def toggle(mac):
     """ Toggle a Wemo device """
+    if mac is None:
+        log.error('MAC address is None!')
+        return
+
     log.info('Toggling WeMo device {}'.format(mac))
+
     return device_toggle(mac)
 
 
